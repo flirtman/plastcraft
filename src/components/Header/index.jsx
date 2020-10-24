17945085195acf1e6c9338c4d864f7a53e01b998
flirtman <iliasvinin@gmail.com>
2020-10-24 13:07:32 -0400

    header started

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
-import {section} from 'react-bootstrap'
-import video from '../../assets/video/production ID_4908396.mp4';
+import { } from 'react-bootstrap'
+import video from '../../assets/video/production_ID_4908396.mp4';
 import classes from './styles.scss';
 import logo from '../../assets/images/logo_814x166.png';
+import {Link} from "react-router-dom";
+
+// https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4
+
 const Header = () => {
     return (
         <header>
             <div className="overlay"></div>
             <video playsinline="playsinline" autoPlay="autoplay" muted="muted" loop="loop">
-                <source src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4"/>
+                <source src={video} type="video/mp4"/>
             </video>
             <div className="container h-100">
                 <div className="d-flex h-100 text-center align-items-center">
                     <div className="w-100 text-white">
-                        <h1 className="display-3">Video Header</h1>
-                        <p className="lead mb-0">With HTML5 Video and Bootstrap 4</p>
+                        <div><img src={logo} alt="" className={'logo'}/></div>
+                        <div>
+                            <ul>
+                                <li><Link to={'/'}>Home</Link></li>
+                                <li><Link to={'/projects'}>Projects</Link></li>
+                                <li><Link to={'/products'}>Products</Link></li>
+                                <li><Link to={'/process'}>Process</Link></li>
+                                <li><Link to={'/about'}>About Us</Link></li>
+                                <li><Link to={'/contact'}>Contact Us</Link></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <p><span className={'txt-green'}>You can change the future,</span><br/>always remember to recycle.</p>
+                        </div>
                     </div>
                 </div>
             </div>
